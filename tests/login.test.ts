bf932fc0ee2205bfa632eb4d5831b0f44bc853a2
win <samihamuhabathulla@gmail.com>
2026-06-25 09:12:38 +0530

    added the demoblaze test file

---
+++ tests/login.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Test', async ({ page }) => {
+    await page.goto(process.env.BASE_URL!);
+    await page.fill('#username', process.env.UNAME!);
+    await page.fill('#password', process.env.PASSWORD!);
+    await page.click('button[type="submit"]');
+    await expect(page.locator('.flash.success')).toBeVisible();
+});                             
--- tests/logindemoblaze.test.ts
+++ tests/logindemoblaze.test.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('Login Test', async ({ page }) => {
+test('LoginDemo Test',async({page})=>{
     await page.goto(process.env.BASE_URL!);
-    await page.fill('#username', process.env.UNAME!);
-    await page.fill('#password', process.env.PASSWORD!);
-    await page.click('button[type="submit"]');
-    await expect(page.locator('.flash.success')).toBeVisible();
-});                             
+    await page.click('#login2')
+    await page.fill('#loginusername',process.env.UNAME!);
+    await page.fill('#loginpassword',process.env.PASSWORD!);
+    await page.click('button[onclick="logIn()"]');
+});
