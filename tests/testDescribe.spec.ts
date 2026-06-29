c7254809a3e519ab121599da6a76607c32d454d2
win <samihamuhabathulla@gmail.com>
2026-06-29 16:46:21 +0530

    fixed the errors

---
+++ tests/testDescribe.spec.ts
@@ -0,0 +1,13 @@
+import { test } from "@playwright/test";
+
+test("Login", async ({ page }) => {
+  console.log("Login test executed");
+});
+
+test("Search", async ({ page }) => {
+  console.log("Search test executed");
+});
+
+test("Logout", async ({ page }) => {
+  console.log("Logout test executed");
+});
